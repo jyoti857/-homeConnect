16c2263386e6e061ff921ea1116b29bf37b6e954
Jyotiranjan767 <jyotiranjan767@gmail.com>
2021-04-11 04:07:46 +0530

    add socket dependency to useEffect

--- src/context/Context.js
+++ src/context/Context.js
@@ -31,7 +31,7 @@ const ContextProvider = ({children}) => {
       socket.on("calluser", ({from, name: callerName, signal})=> {
         setCall({isReceivingCall: true, from, name: callerName, signal})
       });
-  }, []);
+  }, [socket]);
   const answerCall = () => {
     setCallAccepted(true);
 
